c441e6b2af2e20112d0f7f38b68db6bc3b143bb9
Joshua Anderson <JDAnderson0405@gmail.com>
2024-01-03 11:43:41 -0500

    Comments added

--- backend/src/main/java/team/parentpal/localCSVIntegration/CSVIntegration.java
+++ backend/src/main/java/team/parentpal/localCSVIntegration/CSVIntegration.java
@@ -1,3 +1,14 @@
+/*
+ * This class is used to integrate the backend with the local CSV files
+ * @Author: Joshua Anderson
+ * @Version: 1.0
+ * @Date: 1/3/2024
+ * @Last Modified By: ChangeMeIn@UserSettingsUnder.SFDoc
+ * @Last Modified Time: 1/3/2024
+ * @Last Modified Description: 
+ * @Dependencies: com.opencsv.CSVReader, com.opencsv.CSVWriter, com.opencsv.exceptions.CsvException, java.io.FileReader, java.io.* FileWriter, java.io.IOException, java.util.ArrayList, java.util.List
+ */
+
 package team.parentpal.localCSVIntegration;
 
 import java.io.FileReader;
@@ -12,7 +23,13 @@ import com.opencsv.exceptions.CsvException;
 
 public class CSVIntegration {
   /*
+   * Retrieve the CSV file, add the provided record to it, and save it to the
+   * database
    * 
+   * @param csvName: the name of the CSV file to be read - excluding the .csv
+   * extension
+   * 
+   * @param csvRecord: the record to be added to the CSV file
    */
   public boolean saveCSV(String csvName, String[] csvRecord) {
     CSVReader csvReader;
@@ -37,6 +54,10 @@ public class CSVIntegration {
   }
 
   /*
+   * Retrieve the CSV file and return its contents as a list of String arrays
+   * 
+   * @param csvName: the name of the CSV file to be read - excluding the .csv
+   * extension
    * 
    */
   public List<String[]> readCSV(String csvName) {

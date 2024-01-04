fcf32ac3bb3c4dbee9ef44955098d5d6e7132a21
Aki Lawrence <Akilawrencee@gmail.com>
2024-01-04 14:58:40 -0500

    added todo list Api

--- backend/src/main/java/team/parentpal/Controller/BabyFactsController.java
+++ backend/src/main/java/team/parentpal/Controller/BabyFactsController.java
@@ -1,4 +1,4 @@
-package team.parentpal.Controller;
+package team.parentpal.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ backend/src/main/java/team/parentpal/Controller/TodoListController.java
@@ -0,0 +1,27 @@
+package team.parentpal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import team.parentpal.services.TodoListService;
+
+@RestController
+@RequestMapping("/api/todolist")
+public class TodoListController {
+
+    @Autowired
+    private TodoListService todoListService;
+
+    @GetMapping
+    public List<String> getTodoList() {
+        return todoListService.getTodoList();
+    }
+
+    @PostMapping("/addtask")
+    public String addTask(@RequestBody String task) {
+        todoListService.addTask(task);  
+        return "Task added successfully: " + task;
+    }
+}
